1d19ae048400f08c14df3a24006d178c3cc14b1b
julian galvez <julian.galvez@upb.edu.co>
2025-09-30 11:33:06 -0500

    creacion de carpetas y ejercicios

--- listas/ejercicio1.py
+++ listas/ejercicio1.py
@@ -1,8 +1,30 @@
+
+import random
+
 # Lista vacía
 componentes = []
+# agregar un elemento
+elemento = input("ingrese un componente del avion: ")
+componentes. append(elemento)
+print(componentes)
 
-# Lista con elementos
-componentes = ["alas", "fuselaje", "motores", "tren de aterrizaje"]
+
+
+
+
+# Lista numerica 
+numeros = []
+aleatorio = random.randint(0,100)
+numeros. append(aleatorio)
+numeros. append(10)
+#agregar numeros aleatorios a la lista 
+for i in range (10):
+    aleatorio = random.randint(0,100)
+    numeros. append(aleatorio)
+
+print(numeros)
+
+
 
 # Lista con diferentes tipos de datos
 datos_vuelo = [202, "Boeing 737", True, 10500.5]
