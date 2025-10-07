38eba8a117eea3db4b4397e0cc74ab81cb0cd757
julian galvez <julian.galvez@upb.edu.co>
2025-10-07 11:08:57 -0500

    agragar lista y diccionario

---
+++ continuacion_reto/aplicacion_dic_lst.md
@@ -0,0 +1,5 @@
+# diccionario 
+vamos a agregar el diccionario en la parte donde tenemos los valores constantes para trabajar el codigo al inicio, para almacenarlos y luego utilizatrlos en el codigo
+
+# lista
+vamos a agregar una lista vacia, y que se vaya llenando deacuerdo al bucle que cada que se ejecute ocupe un espacio de la lista y a lo ultimo nombrarla para mostrar los resultados del codigo 
---
+++ continuacion_reto/codigo.py
@@ -0,0 +1,66 @@
+
+
+Te_inicial = float(input("Ingrese la temperatura inicial (°C): "))
+tiempo_vuelo = float(input("Ingrese el tiempo de vuelo (minutos): "))
+valores = {
+    "Te_min": 30,
+    "Te_max": 80,
+    "intervalo_1": 10,
+    "tiempo_estabilizacion": 5,
+    "intervalo_2": 2
+}
+Te_actual = Te_inicial
+
+print("1. Boeing 747-8 (k = 5)")
+print("2. Airbus A380-800 (k = 8)")
+print("3. Antonov An-225 (k = 10)")
+
+opcion = int(input("Seleccione el avión: "))
+
+match opcion:
+    case 1:
+        print("1. Boeing 747-8 (k = 5)")
+        k = 5
+    case 2:
+        print("2. Airbus A380-800 (k = 8)")
+        k = 8
+    case 3:
+        print("3. Antonov An-225 (k = 10)")
+        k = 10
+    case _:
+        print("Opción no válida")
+
+
+tiempo_inicio_estabilizacion = tiempo_vuelo - valores["tiempo_estabilizacion"]
+tiempo_actual = 0
+while tiempo_actual < tiempo_vuelo:
+    tiempo_actual += valores["intervalo_1"]
+
+    dT_dt = -k * (Te_actual - Te_inicial)
+    Te_nueva = Te_actual + (dT_dt * (valores["intervalo_1"] / 60))
+    Te_actual = Te_nueva
+
+    if tiempo_actual < tiempo_inicio_estabilizacion:
+        if (int(tiempo_actual / valores["intervalo_1"])) % 2 == 0:
+            Te_actual += 5
+        else:
+            Te_actual -= 5
+
+    if Te_actual < valores["Te_max"]:
+        print("La temperatura del LG es menor que la temperatura máxima, verificar si es menor que la mínima")
+        if Te_actual < valores["Te_min"]:
+            print("Cuidado: temperatura muy fría")
+            Te_actual += (-k * (Te_actual - Te_inicial)) * (valores["intervalo_2"] / 60)
+        else:
+            print("Temperatura en rango ideal")
+    else:
+        print("temperatura muy caliente")
+        Te_actual -= (-k * (Te_actual - Te_inicial)) * (valores["intervalo_2"] / 60)
+
+    if tiempo_actual >= tiempo_inicio_estabilizacion:
+        print("Fase de estabilización, asegurar para aterrizar")
+    else:
+        print("No iniciar fase de estabilización, seguir monitoreando.")
+
+    print(f"Temperatura actual: {Te_actual:.2f} °C")
+
